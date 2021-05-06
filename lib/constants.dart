fbae4985b38f62b5d1142156c366b6f5964478a2
Doflamingo <vaishnavgaikwad721@gmail.com>
2021-05-06 13:58:52 +0530

    auth: implemented refresh token

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,5 @@
 // Add all the constants here
+import 'package:flutter/material.dart';
+
+const kBaseUrl = 'http://52.136.112.220:57842/api';
+const kTimeDiff = 50;
--- lib/src/api/login_api_service.dart
+++ lib/src/api/login_api_service.dart
@@ -1,29 +1,60 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:final_app/constants.dart';
 
 class APIService {
   Future<Map> login(String userId, String password) async {
     dynamic postBody = <String, String>{"userId": userId, "password": password};
-    String url = "http://52.136.112.220:57842/api/auth/login";
+
+    String url = "$kBaseUrl/auth/login";
     var headers = {'Content-Type': 'application/json'};
     SharedPreferences prefs = await SharedPreferences.getInstance();
     var jsonResponse = null;
     int timeStamp = DateTime.now().millisecondsSinceEpoch;
-    // print(postBody);
+
     http.Response response = await http.post(Uri.parse(url),
         body: jsonEncode(postBody), headers: headers);
+
     if (response.statusCode == 200) {
       jsonResponse = jsonDecode(response.body);
-      //print(postBody);
       prefs.setString("token", jsonResponse['token']);
       prefs.setString('refreshToken', jsonResponse['refreshToken']);
       prefs.setInt('timeStamp', timeStamp);
-      print(jsonResponse['refreshToken']);
-      print(prefs.getInt('timeStamp'));
+      //print(jsonResponse['refreshToken']);
       return {'error': false};
     } else {
       return {'error': true};
     }
+  }
+
+  Future<Map> getToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString('token')!;
+    String refreshToken = prefs.getString('refreshToken')!;
+    dynamic postBody = <String, String>{
+      "token": token,
+      "refreshToken": refreshToken
+    };
+    String url = "$kBaseUrl/auth/refresh";
+    var headers = {
+      'Authentication': 'Bearer $token',
+      'Content-Type': 'application/json'
+    };
+
+    int tokenTime = prefs.getInt('timeStamp')!;
+    DateTime time = DateTime.fromMillisecondsSinceEpoch(tokenTime);
+    var currentTime = DateTime.now();
+    Duration timeDiff = currentTime.difference(time);
+    int minutes = timeDiff.inMinutes;
+    var jsonResponse = null;
+    if (minutes > kTimeDiff) {
+      http.Response response = await http.post(Uri.parse(url),
+          body: jsonEncode(postBody), headers: headers);
+      jsonResponse = jsonDecode(response.body);
+      print(jsonResponse);
+      return {token: jsonResponse['token']};
+    }
+    return {token: null};
   }
 }
